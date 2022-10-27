99aa099bff219bd0749e753a0a767cd17109114c
Marduk Bolaños <mardukbp@mac.com>
2022-10-28 00:06:32 +0200

    First commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldTerminate()
+    {
+        int j = 0;
+        for(int i=0;i <1; j++){}
+    }
+}
